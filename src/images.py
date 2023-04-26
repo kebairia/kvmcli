600b27374620967966a10cbd447356036280b0a2
Zakaria Kebairia <4.kebairia@gmail.com>
2023-04-26 19:52:46 +0100

    Fixing and cleaning images.py

--- src/images.py
+++ src/images.py
@@ -1,11 +1,12 @@
 #!/bin/python
-import os
-import time
 import shutil
+import logging
 from pathlib import Path
 
 from config import config
-from src.misc import print_message
+# from src.misc import print_message
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 
 def copy_image(name: str, vm: dict) -> dict:
@@ -22,23 +23,29 @@ def copy_image(name: str, vm: dict) -> dict:
     """
     images_path = config.IMAGES_PATH
     if images_path is None:
-        print_message("ERROR", f"Could not find the default images path --> {default_images_path}")
+        logging.error(
+            f"Could not find the default images path --> {images_path}")
         return {'dest_image': None, 'source_image_exists': False, 'dest_image_exists': False}
 
-    dest_image = Path(images_path) / f"{name}.{vm['storage']['disk']['format']}"
-    source_image = Path(config.ARTIFACTS_PATH) / f"{vm['info']['image']}.{vm['storage']['disk']['format']}"
+    dest_image = Path(images_path) / \
+        f"{name}.{vm['storage']['disk']['format']}"
+
+    source_image = Path(config.ARTIFACTS_PATH) / \
+        f"{vm['info']['image']}.{vm['storage']['disk']['format']}"
+
     if not dest_image.exists():
         if source_image.exists():
             try:
-                print_message("INFO", f"Copying new VM to {dest_image}")
-                shutil.copy2(str(source_image), str(dest_image))
+                logging.info(f"Copying new VM to {dest_image}")
+                shutil.copy(str(source_image), str(dest_image))
             except shutil.SameFileError:
-                print_message("WARNING", f"File already exists at {dest_image}. Skipping copy.")
+                logging.warning(
+                    f"File already exists at {dest_image}. Skipping copy.")
         else:
-            print_message("ERROR", f"File not found - {source_image}")
+            logging.error(f"File not found - {source_image}")
             return {'dest_image': None, 'source_image_exists': False, 'dest_image_exists': False}
     else:
-        print_message("ERROR", f"File already exists at {dest_image}. Skipping copy.")
+        logging.error(f"File already exists at {dest_image}. Skipping copy.")
         return {'dest_image': None, 'source_image_exists': False, 'dest_image_exists': False}
 
-    return {'dest_image': str(dest_image), 'source_image_exists': source_image.exists(), 'dest_image_exists': dest_image.exists() }
+    return {'dest_image': str(dest_image), 'source_image_exists': source_image.exists(), 'dest_image_exists': dest_image.exists()}
