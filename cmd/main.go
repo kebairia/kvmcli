aaceb909ed06f1e7101ac4ba3f5f2fd164611110
Zakaria Kebairia <4.kebairia@gmail.com>
2025-03-15 20:12:58 +0100

    nothing important

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/kebairia/kvmcli/internal/utils"
+	op "github.com/kebairia/kvmcli/internal/operations"
 )
 
 func main() {
-	domain := utils.NewDomain(
-		"code-test",
-		5120,
-		2,
-		"/home/zakaria/dox/homelab/artifacts/rocky-linux-9.qcow2",
-		"52:54:00:e9:a9:29",
-	)
-	xmlOutput, _ := domain.GenerateXML()
-	fmt.Println(string(xmlOutput))
+	op.ProvisionVMs("/home/zakaria/dox/homelab/kvmcli/servers.yaml")
 }
