e5a3cf7a86207883f140873c62ab164a3ac08363
Zakaria Kebairia <4.kebairia@gmail.com>
2025-03-15 23:49:16 +0100

    feat(main): Add flags

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"flag"
+
 	op "github.com/kebairia/kvmcli/internal/operations"
 )
 
 func main() {
-	op.ProvisionVMs("/home/zakaria/dox/homelab/kvmcli/servers.yaml")
+	// Declare the config flag with a default value ("servers.yaml") and a short description.
+	configPath := flag.String("config", "servers.yaml", "Path to the YAML configuration file")
+	// Declare a boolean flag (e.g., --verbose) if you need more logging or debugging info.
+	// verbose := flag.Bool("verbose", false, "Enable verbose logging")
+
+	// Parse all the flags. This reads os.Args and sets the values of the flag variables.
+	flag.Parse()
+
+	op.ProvisionVMs(*configPath)
 }
