8f779850a1ec1c280e0daaa5604e18dbd53aab5c
Zakaria Kebairia <4.kebairia@gmail.com>
2025-03-16 23:18:50 +0100

    add(main.go, root.go): Add basic command line

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
+	"github.com/kebairia/kvmcli/internal/logger"
 	"github.com/spf13/cobra"
 )
 
@@ -15,49 +13,21 @@ var (
 	Verbose     bool   // To determine if output should be in verbose
 )
 
+// Create the root command
+var rootCmd = &cobra.Command{
+	Use:   "kvmcli",
+	Short: "kvmcli is a CLI for managing KVM virtual machines",
+	Long:  "A CLI similar to kubectl for creating, deleting, and managing KVM VMs.",
+}
+
 func Execute() {
-	// Create the root command
-	rootCmd := &cobra.Command{
-		Use:   "kvmcli",                                 // How the command is called from the terminal
-		Short: "kvmcli is a tool to manage VM clusters", // A short description
-		Long: `
-    __                        ___ 
-   / /___   ______ ___  _____/ (_)
-  / //_/ | / / __ '__ \/ ___/ / / 
- / ,<  | |/ / / / / / / /__/ / /  
-/_/|_| |___/_/ /_/ /_/\___/_/_/
-
-kvmcli is a command line tool that helps you manage and provision 
-virtual machine clusters using configuration files. `,
-		// The Run function executes when the root command is called without any subcommands.
-		Run: func(cmd *cobra.Command, args []string) {
-			// Here you could call further functions to load configuration, read cluster file, and provision VMs.
-		},
-	}
-
-	// Define flags for the root command using PersistentFlags.
-	// Persistent flags are available to the command and all its subcommands.
-	// If you wanted flags only for this command, you could use Flags() instead.
-
-	// 1. --config or -c: specifies the path to the config file.
-	rootCmd.PersistentFlags().
-		StringVarP(&ConfigFile, "config", "c", "config.toml", "Path to the configuration file")
-
-	// 2. --file or -f: specifies the file that defines your cluster info.
-	rootCmd.PersistentFlags().
-		StringVarP(&ClusterFile, "file", "f", "servers.yaml", "Path to the cluster file")
-
-	// 3. --provision or -p: a boolean flag to start provisioning VMs.
-	rootCmd.PersistentFlags().
-		BoolVarP(&Provision, "provision", "p", false, "Start provisioning VMs")
-
-	// 4. --verbose or -v: a boolean flag to start provisioning VMs.
-	rootCmd.PersistentFlags().
-		BoolVarP(&Verbose, "verbose", "v", false, "Output in verbose mode")
-
-	// Execute the command line application.
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
+		logger.Log.Errorf("%s", err)
 	}
+}
+
+func init() {
+	rootCmd.AddCommand(CreateCmd)
+	rootCmd.AddCommand(DeleteCmd)
+	rootCmd.AddCommand(GetCmd)
 }
