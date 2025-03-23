61f622eb50955c430a5ac28c7c1dfb41e37d0710
Zakaria Kebairia <4.kebairia@gmail.com>
2025-03-24 00:04:49 +0100

    add(loader): Now I load any manifest file and filter according to kind

---
+++ internal/loader/manifest_loader.go
@@ -0,0 +1,61 @@
+package loader
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/kebairia/kvmcli/internal/resources"
+	"github.com/kebairia/kvmcli/internal/store"
+	"github.com/kebairia/kvmcli/internal/vms"
+	"gopkg.in/yaml.v3"
+)
+
+func LoadManifest(manifestPath string) ([]resources.Resource, error) {
+	var resourceList []resources.Resource
+	file, err := os.Open(manifestPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	for {
+		var rawMap map[string]any
+		if err := decoder.Decode(&rawMap); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, fmt.Errorf("failed to decode YAML: %w", err)
+		}
+		kind, ok := rawMap["kind"].(string)
+		if !ok {
+			return nil, fmt.Errorf("missing or invalide 'kind' field")
+		}
+		rawBytes, err := yaml.Marshal(rawMap)
+		if err != nil {
+			return nil, fmt.Errorf("failed to re-marshal resource: %w", err)
+		}
+		switch kind {
+		// Virtual Machines
+		case "VirtualMachine":
+			var vm vms.VirtualMachine
+			if err := yaml.Unmarshal(rawBytes, &vm); err != nil {
+				return nil, fmt.Errorf("failed to unmarshal VirtualMachine: %w", err)
+			}
+			resourceList = append(resourceList, &vm)
+			// Stores
+		case "Store":
+			var store store.Store
+			if err := yaml.Unmarshal(rawBytes, &store); err != nil {
+				return nil, fmt.Errorf("failed to unmarshal Store: %w", err)
+			}
+			resourceList = append(resourceList, &store)
+
+			// Default action
+		default:
+			return nil, fmt.Errorf("unknown kind: %s", kind)
+		}
+	}
+	return resourceList, nil
+}
