e125334dc104a46c06e0e3f174f3bc66d20593d7
Zakaria Kebairia <4.kebairia@gmail.com>
2025-04-01 16:10:27 +0100

    add(resources): Add Record interface

--- internal/resources/resource.go
+++ internal/resources/resource.go
@@ -8,6 +8,12 @@ type Resource interface {
 	Delete() error
 }
 
+// Record defines operations for managing a records on database.
+type Record interface {
+	Insert() error
+	Delete() error
+}
+
 // ClientSetter is implemented by types that require a libvirt connection.
 type ClientSetter interface {
 	SetConnection(*libvirt.Libvirt)
