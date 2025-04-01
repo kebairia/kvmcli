c5362870e4a1f40254b2f5ead1628e8bfe472eb6
Zakaria Kebairia <4.kebairia@gmail.com>
2025-04-01 14:01:38 +0100

    add(database): InsertStore() function

--- internal/database/insert.go
+++ internal/database/insert.go
@@ -38,3 +38,19 @@ func InsertNet(record *NetRecord) (primitive.ObjectID, error) {
 	logger.Log.Debugf("Inserted Network record with _id: %v", result.InsertedID)
 	return insertedID, nil
 }
+
+func InsertStore(record *StoreRecord) (primitive.ObjectID, error) {
+	record.ID = primitive.NewObjectID()
+	collection := client.Database("kvmcli").Collection(StoreCollection)
+	result, err := collection.InsertOne(ctx, record)
+	if err != nil {
+		return primitive.NilObjectID, fmt.Errorf("Insert record failed: %w", err)
+	}
+	insertedID, ok := result.InsertedID.(primitive.ObjectID)
+	if !ok {
+		return primitive.NilObjectID, fmt.Errorf("InsertedID is not an ObjectID")
+	}
+
+	logger.Log.Debugf("Inserted store record with _id: %v", result.InsertedID)
+	return insertedID, nil
+}
