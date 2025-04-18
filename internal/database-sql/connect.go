a7577d525e4b1d50973a873a617420e6fda6e0b8
Zakaria Kebairia <4.kebairia@gmail.com>
2025-04-19 00:54:59 +0100

    refactor(sql): connection function

--- internal/database-sql/connect.go
+++ internal/database-sql/connect.go
@@ -8,12 +8,20 @@ import (
 	_ "github.com/mattn/go-sqlite3" // SQLite driver
 )
 
-// Database file name
-const dbFileName = "/home/zakaria/dox/homelab/kvmcli/kvmcli.db"
+// Constants
+const (
+	DBFilePath     = "/home/zakaria/dox/homelab/kvmcli/kvmcli.db"
+	DatabaseName   = "kvmcli"
+	StoreTable     = "store"
+	VMsTable       = "vms"
+	NetworksTable  = "networks"
+	SnapshotsTable = "snapshots"
+)
 
+// Global shared variables (only if you decide to use globals).
 var (
 	DB  *sql.DB
-	Ctx context.Context
+	Ctx = context.Background()
 )
 
 // InitDB opens a database handle and verifies the connection using context.
@@ -22,23 +30,18 @@ func InitDB() (*sql.DB, error) {
 	// Define a 5-second timeout context for DB operations
 	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	// defer cancel()
-	Ctx = context.Background() // ← Persistent context for app lifetime
 
 	// Open a handle to the SQLite database (does not connect yet)
 	var err error
-	DB, err = sql.Open("sqlite3", dbFileName)
-	if DB == nil {
-		return nil, fmt.Errorf("database connection is nil")
-	}
+	db, err := sql.Open("sqlite3", DBFilePath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open DB handle: %w", err)
 	}
 
 	// Verify the connection with PingContext (this actually connects)
-	if err := DB.PingContext(Ctx); err != nil {
+	if err := db.PingContext(Ctx); err != nil {
 		return nil, fmt.Errorf("failed to connect to DB: %w", err)
 	}
-	fmt.Println("Connected successfully ")
 
-	return DB, nil
+	return db, nil
 }
