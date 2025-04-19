9a544106529752af9da93615f7e53c01ba51c301
Zakaria Kebairia <4.kebairia@gmail.com>
2025-04-19 01:01:35 +0100

    clean , remove some network functions for some thing better

--- internal/database-sql/types.go
+++ internal/database-sql/types.go
@@ -51,31 +51,3 @@ func EnsureVMTable(ctx context.Context, db *sql.DB) error {
 	}
 	return nil
 }
-
-// EnsureVMTable creates the vms table if it doesn't exist.
-func EnsureNetworkTable(ctx context.Context, db *sql.DB) error {
-	const schema = `
-  CREATE TABLE IF NOT EXISTS networks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    namespace TEXT,
-    labels TEXT,
-    mac_address TEXT,
-    bridge TEXT,
-    mode TEXT,
-    net_address TEXT,
-    netmask TEXT,
-    dhcp TEXT,
-    autostart BOOLEAN,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  );
-
-	CREATE UNIQUE INDEX IF NOT EXISTS idx_net_name_namespace ON networks(name, namespace);
-
-	`
-	_, err := db.ExecContext(ctx, schema)
-	if err != nil {
-		return fmt.Errorf("failed to create networks table: %w", err)
-	}
-	return nil
-}
