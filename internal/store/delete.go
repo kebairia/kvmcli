e6c0fafbafedc3e3ada20f900e884cfb428b1c20
Zakaria Kebairia <4.kebairia@gmail.com>
2025-05-29 23:54:06 +0100

    add(store): Add Delete function

--- internal/store/delete.go
+++ internal/store/delete.go
@@ -1,8 +1,19 @@
 package store
 
-import "fmt"
+import (
+	"fmt"
+
+	db "github.com/kebairia/kvmcli/internal/database"
+	"github.com/kebairia/kvmcli/internal/logger"
+)
 
 func (s *Store) Delete() error {
-	fmt.Println("Delete a store object")
+	name := s.Metadata.Name
+	// Delete the store record from the database.
+	if err := db.Delete(db.Ctx, db.DB, name, db.StoreTable); err != nil {
+		logger.Log.Errorf("failed to delete record for store %q: %v", name, err)
+	}
+
+	fmt.Printf("store/%s deleted\n", s.Metadata.Name)
 	return nil
 }
