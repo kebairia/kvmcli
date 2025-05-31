f5d4146c196e817a61c67a48c7690fc92b66af76
Zakaria Kebairia <4.kebairia@gmail.com>
2025-05-31 11:45:07 +0100

    clean: the main.go file

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/kebairia/kvmcli/cmd"
 	db "github.com/kebairia/kvmcli/internal/database"
-	"github.com/kebairia/kvmcli/internal/logger"
 )
 
 func main() {
 	var err error
 	db.DB, err = db.InitDB()
 	if err != nil {
-		logger.Log.Fatalf("DB initialization failed: %v", err)
+		log.Fatalf("DB initialization failed: %v", err)
 	}
 	// Execute the CLI commands defined in the cmd package.
 	cmd.Execute()
