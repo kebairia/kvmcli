c9023695fa059502c5144f37ea73c82995d8bd45
Zakaria Kebairia <4.kebairia@gmail.com>
2025-06-02 16:37:14 +0100

    fix: return error message instead of log the error message

--- internal/store/delete.go
+++ internal/store/delete.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 
 	db "github.com/kebairia/kvmcli/internal/database"
-	log "github.com/kebairia/kvmcli/internal/logger"
 )
 
 func (s *Store) Delete() error {
 	name := s.Metadata.Name
 	// Delete the store record from the database.
 	if err := db.Delete(db.Ctx, db.DB, name, db.StoreTable); err != nil {
-		log.Errorf("failed to delete record for store %q: %v", name, err)
+		return fmt.Errorf("failed to delete record for store %q: %v", name, err)
 	}
 
 	fmt.Printf("store/%s deleted\n", s.Metadata.Name)
