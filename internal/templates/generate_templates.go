9cd4dc3a973ead07ffd26ff9b7f3e4e4bfc85340
Zakaria Kebairia <4.kebairia@gmail.com>
2025-12-13 00:18:13 +0100

    refactor(templates): move XML definitions from utils to templates package
    - Move xml_vm_definition.go and xml_net_definition.go to internal/templates/
    - Move generate_templates.go, header.go, helpers.go to internal/templates/
    - Delete internal/utils/ package (no longer needed)

--- internal/templates/generate_templates.go
+++ internal/templates/generate_templates.go
@@ -1,4 +1,4 @@
-package utils
+package templates
 
 func GenerateYAMLTemplate() {
 	// sampleConfig := config.VMConfig{
--- internal/templates/xml_vm_definition.go
+++ internal/templates/xml_vm_definition.go
@@ -1,4 +1,4 @@
-package utils
+package templates
 
 import (
 	"encoding/xml"
